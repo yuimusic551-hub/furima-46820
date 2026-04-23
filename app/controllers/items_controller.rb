32f0200fd7e64a54292c023cb42bca505e8782a2
yuimusic551-hub <yuimusic551@gmail.com>
2026-04-23 17:07:44 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,7 @@
 class ItemsController < ApplicationController
-  # ログインしていないユーザーをログイン画面へ飛ばす（indexとshow以外）
   before_action :authenticate_user!, except: [:index, :show]
-  # 指定のアクションの時だけ、事前に商品データを取得する
-  # before_action :set_item, only: [:show, :edit, :update, :destroy]
-  # 出品者本人でない場合はトップページへ戻す（編集・更新・削除のみ）
-  # before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -24,22 +21,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
-  # def edit
-  # すでに set_item で @item が作られているので、中身は空でOK
-  # end
+  def edit
+    # @item = Item.find(params[:id])
+  end
 
-  # def update
-  # if @item.update(item_params)
-  # 更新に成功したら詳細画面へ戻る
-  # redirect_to item_path(@item.id)
-  # else
-  # 失敗したら編集画面を再表示
-  # render :edit, status: :unprocessable_entity
-  # end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   # def destroy
   # 商品を削除してトップページへ戻る
@@ -56,13 +52,12 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  # @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
-  # def move_to_index
-  # return if current_user.id == @item.user_id
-
-  # redirect_to root_path
-  # end
+  def move_to_index
+    return if current_user.id == @item.user_id
+    redirect_to root_path
+  end
 end
