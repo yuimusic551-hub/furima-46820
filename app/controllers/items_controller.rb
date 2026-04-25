68725ca7ebf25ecde1fceb808edb5ae5ef2140e6
yuimusic551-hub <yuimusic551@gmail.com>
2026-04-25 20:22:31 +0900

    削除機能の任意修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,11 +26,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # @item = Item.find(params[:id])
   end
 
   def edit
-    # @item = Item.find(params[:id])
   end
 
   def update
